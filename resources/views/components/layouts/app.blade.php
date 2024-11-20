bd38871eb4dd7e0f15e7dcf9b8bb61ba43e96829
agriapriliando <agripriliando@gmail.com>
2024-11-20 07:48:58 +0700

    pilih berkas by company search

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -232,9 +232,6 @@
 
     <!-- Kaiadmin JS -->
     <script src="{{ asset('') }}assets/js/kaiadmin.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    @stack('scripts')
 </body>
 
 </html>
